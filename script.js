0c75506cf8ac057c210d7b34e3c6770c14129e25
Kunnikar <kunnikarboon@gmail.com>
2024-04-12 10:43:10 +0200

    add method addTask() to adds a new task to the tasks array

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ const taskManager = {
   // an array that hold all tasks.
   tasks: [],
   idCounter: 1,
-  
+// Method adds a new task to the tasks array
+addTask: function (description) {
+  // Remove whitespace then chcek if task is empty or a number
+  if ((description.trim() === "") || (!isNaN(description))) {
+    alert('Task can not be empty or a number!' );
+    const taskDescrition = prompt('Add task description, then click OK:');
+    taskManager.addTask(taskDescrition);
+    return;
+  }
+    
